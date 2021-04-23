7047c7465e95f2a508ebc4dc0486921461087bd2
= <ganiyusodiq059@gmail.com>
2021-04-23 10:49:05 -1200

    set all links url

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,32 @@
-
-
 $(function () {
-  let $animation_elements = $('#about');
-  let $student = $('.student');
-  let $teacher = $('.teacher');
-  let $award = $('.award');
-  let $window = $(window);
+  // let $animation_elements = $('#about');
+  // let $student = $('.student');
+  // let $teacher = $('.teacher');
+  // let $award = $('.award');
+  // let $window = $(window);
 
-  function check_if_in_view() {
-    let window_height = $window.height();
-    let window_top_position = $window.scrollTop();
-    let window_bottom_position = (window_top_position + window_height);
+  // function check_if_in_view() {
+  //   let window_height = $window.height();
+  //   let window_top_position = $window.scrollTop();
+  //   let window_bottom_position = (window_top_position + window_height);
 
-    let element_height = $animation_elements.outerHeight();
-    let element_top_position = $animation_elements.offset().top;
-    let element_bottom_position = (element_top_position + element_height);
+  //   let element_height = $animation_elements.outerHeight();
+  //   let element_top_position = $animation_elements.offset().top;
+  //   let element_bottom_position = (element_top_position + element_height);
 
-    //check to see if this current container is within viewport
-    if ((element_bottom_position >= window_top_position) &&
-      (element_top_position <= window_bottom_position)) {
-      $student.addClass('student-counter');
-      $teacher.addClass('teacher-counter');
-      $award.addClass('award-counter');
-    } else {
-      // $student.removeClass('student-counter');
-      // $teacher.removeClass('teacher-counter');
-      // $award.removeClass('award-counter');
-    }
-  }
+    
+  //   if ((element_bottom_position >= window_top_position) &&
+  //     (element_top_position <= window_bottom_position)) {
+  //     $student.addClass('student-counter');
+  //     $teacher.addClass('teacher-counter');
+  //     $award.addClass('award-counter');
+  //   } else {
+      
+  //   }
+  // }
 
-  $window.on('scroll resize', check_if_in_view);
-  $window.trigger('scroll');
+  // $window.on('scroll resize', check_if_in_view);
+  // $window.trigger('scroll');
 
   //navbar toggle
   navToggle();
